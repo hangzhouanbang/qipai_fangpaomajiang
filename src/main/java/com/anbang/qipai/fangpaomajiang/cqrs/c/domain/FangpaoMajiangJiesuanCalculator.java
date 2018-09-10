ccd242a0e2e3ce1852f67a14e159b6f503b36285
林少聪 < @PC-20180515PRDG>
2018-09-10 09:11:33 +0800

    通用化当牌

--- src/main/java/com/anbang/qipai/fangpaomajiang/cqrs/c/domain/FangpaoMajiangJiesuanCalculator.java
+++ src/main/java/com/anbang/qipai/fangpaomajiang/cqrs/c/domain/FangpaoMajiangJiesuanCalculator.java
@@ -388,7 +388,7 @@ public class FangpaoMajiangJiesuanCalculator {
 		// 计算牌型
 		List<PaiXing> paiXingList = shoupaiCalculator.calculateAllPaiXingFromGouXing(gouXing);
 		for (PaiXing paiXing : paiXingList) {
-			List<ShoupaiPaiXing> shoupaiPaiXingList = paiXing.generateShoupaiPaiXingByGuipaiDangPai(guipaiDangPaiArray);
+			List<ShoupaiPaiXing> shoupaiPaiXingList = paiXing.generateShoupaiPaiXingByDangPai(guipaiDangPaiArray);
 			// 过滤暗杠或暗刻有两个财神当的
 			Iterator<ShoupaiPaiXing> i = shoupaiPaiXingList.iterator();
 			while (i.hasNext()) {
@@ -425,7 +425,7 @@ public class FangpaoMajiangJiesuanCalculator {
 		// 计算牌型
 		List<PaiXing> paiXingList = shoupaiCalculator.calculateAllPaiXingFromGouXing(gouXing);
 		for (PaiXing paiXing : paiXingList) {
-			List<ShoupaiPaiXing> shoupaiPaiXingList = paiXing.generateShoupaiPaiXingByGuipaiDangPai(guipaiDangPaiArray);
+			List<ShoupaiPaiXing> shoupaiPaiXingList = paiXing.generateShoupaiPaiXingByDangPai(guipaiDangPaiArray);
 			// 过滤暗杠或暗刻有两个财神当的
 			Iterator<ShoupaiPaiXing> i = shoupaiPaiXingList.iterator();
 			while (i.hasNext()) {
