bf898132d4cba92e5f0dc19465a91fcc5ea4b9c9
林少聪 < @PC-20180515PRDG>
2018-09-21 23:54:39 +0800

    投票解散状态修改

--- src/main/java/com/anbang/qipai/fangpaomajiang/web/controller/MajiangController.java
+++ src/main/java/com/anbang/qipai/fangpaomajiang/web/controller/MajiangController.java
@@ -220,14 +220,15 @@ public class MajiangController {
 		if (firstActionFrame != null) {
 			queryScopes.add(QueryScope.panForMe);
 		}
+		// 通知其他人
 		for (String otherPlayerId : readyToNextPanResult.getMajiangGame().allPlayerIds()) {
 			if (!otherPlayerId.equals(playerId)) {
-				QueryScope
-						.scopesForState(readyToNextPanResult.getMajiangGame().getState(),
-								readyToNextPanResult.getMajiangGame().findPlayerState(otherPlayerId))
-						.forEach((scope) -> {
-							wsNotifier.notifyToQuery(otherPlayerId, scope.name());
-						});
+				List<QueryScope> scopes = QueryScope.scopesForState(readyToNextPanResult.getMajiangGame().getState(),
+						readyToNextPanResult.getMajiangGame().findPlayerState(otherPlayerId));
+				scopes.remove(QueryScope.panResult);
+				scopes.forEach((scope) -> {
+					wsNotifier.notifyToQuery(otherPlayerId, scope.name());
+				});
 			}
 		}
 		data.put("queryScopes", queryScopes);
