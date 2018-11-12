b281f83766dbd7b8e5e6dca40ee93a2914843652
林少聪 < @PC-20180515PRDG>
2018-11-12 17:03:20 +0800

    俏皮话转义、bugfix

--- src/main/java/com/anbang/qipai/fangpaomajiang/conf/FilePathConfig.java
+++ src/main/java/com/anbang/qipai/fangpaomajiang/conf/FilePathConfig.java
@@ -9,6 +9,10 @@ import org.springframework.stereotype.Component;
 @PropertySource(value = "classpath:application.yml", factory = YamlPropertySourceFactory.class)
 public class FilePathConfig {
 
+	// private String snapshotFileBasePath = "D://qipai/qipai_fangpaomajiang_snap";
+	//
+	// private String jFileBasePath = "D://qipai/qipai_fangpaomajiang";
+
 	private String snapshotFileBasePath;
 
 	private String jFileBasePath;
