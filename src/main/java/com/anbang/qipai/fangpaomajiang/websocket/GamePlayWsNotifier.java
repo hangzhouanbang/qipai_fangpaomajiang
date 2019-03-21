fb71bcb515e734317f6d7fa75f3cc335b6fd7276
林少聪 < @PC-20180515PRDG>
2019-03-21 18:43:11 +0800

    bugfix

--- src/main/java/com/anbang/qipai/fangpaomajiang/websocket/GamePlayWsNotifier.java
+++ src/main/java/com/anbang/qipai/fangpaomajiang/websocket/GamePlayWsNotifier.java
@@ -274,7 +274,7 @@ public class GamePlayWsNotifier {
 	}
 
 	public boolean hasSessionForPlayer(String playerId) {
-		return playerIdSessionIdMap.get(playerId) != null;
+		return playerIdSessionIdMap.containsKey(playerId);
 	}
 
 }
